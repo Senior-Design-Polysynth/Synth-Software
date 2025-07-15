b5d7a6657e42d78658d3db4f73f85ded1a1be5f6
Erzire <rbrosario13@gmail.com>
2025-07-15 14:55:46 -0400

    Two-oscillator test code plus Daisy integration

--- Function Generator/oscillator.cpp
+++ Function Generator/oscillator.cpp
@@ -6,44 +6,65 @@ using namespace daisysp;
 using namespace seed;
 
 DaisySeed hw;
-Oscillator osc;
-AdcChannelConfig adcConfig[3];
+Oscillator osc1, osc2;
+AdcChannelConfig adcConfig[6];  // 6 controls
 
-float volume = 0.f;
-float pitch = 0.f;
-float pulseW = 0.f;
-int currentWaveform = 0;
-bool lastButtonState = false;
+float volume1 = 0.f, volume2 = 0.f;
+float pitch1 = 0.f, pitch2 = 0.f;
+float pulseW1 = 0.f, pulseW2 = 0.f;  // Separate PWM for each oscillator
+int currentWaveform1 = 0, currentWaveform2 = 0;  // Separate waveform states
+bool lastButtonState1 = false, lastButtonState2 = false;  // Separate button states
 
 void AudioCallback(AudioHandle::InputBuffer in,
                   AudioHandle::OutputBuffer out,
                   size_t size)
 {
-    volume = hw.adc.GetFloat(0);
-    pitch = hw.adc.GetFloat(1);
-    pulseW = hw.adc.GetFloat(2);
+    // Read all potentiometers
+    volume1 = hw.adc.GetFloat(0);  // OSC1 volume
+    pitch1 = hw.adc.GetFloat(1);   // OSC1 pitch
+    pulseW1 = hw.adc.GetFloat(2);  // OSC1 pulse width
+    volume2 = hw.adc.GetFloat(3);  // OSC2 volume
+    pitch2 = hw.adc.GetFloat(4);   // OSC2 pitch
+    pulseW2 = hw.adc.GetFloat(5);  // OSC2 pulse width
 
+    // Configure oscillator 1
+    osc1.SetFreq(50.f + (pitch1 * 1950.f));
+    osc1.SetAmp(volume1);
+    osc1.SetPw(pulseW1);
 
-    osc.SetFreq(50.f + (pitch * 1950.f));
-    osc.SetAmp(volume);
-    osc.SetPw(pulseW);
+    // Configure oscillator 2
+    osc2.SetFreq(50.f + (pitch2 * 1950.f));
+    osc2.SetAmp(volume2);
+    osc2.SetPw(pulseW2);
 
     for(size_t i = 0; i < size; i++)
     {
-        float sig = osc.Process();
-        out[0][i] = sig;
-        out[1][i] = sig;
+        float sig1 = osc1.Process();
+        float sig2 = osc2.Process();
+        out[0][i] = sig1 + sig2;
+        out[1][i] = sig1 + sig2;
     }
 }
 
-void UpdateWaveform()
+void UpdateWaveform1()
 {
-    currentWaveform = (currentWaveform + 1) % 3;
-    switch(currentWaveform)
+    currentWaveform1 = (currentWaveform1 + 1) % 3;
+    switch(currentWaveform1)
     {
-        case 0: osc.SetWaveform(Oscillator::WAVE_POLYBLEP_SQUARE); break;
-        case 1: osc.SetWaveform(Oscillator::WAVE_POLYBLEP_SAW); break;
-        case 2: osc.SetWaveform(Oscillator::WAVE_POLYBLEP_TRI); break;
+        case 0: osc1.SetWaveform(Oscillator::WAVE_POLYBLEP_SQUARE); break;
+        case 1: osc1.SetWaveform(Oscillator::WAVE_POLYBLEP_SAW); break;
+        case 2: osc1.SetWaveform(Oscillator::WAVE_POLYBLEP_TRI); break;
+    }
+}
+
+void UpdateWaveform2()
+{
+    currentWaveform2 = (currentWaveform2 + 1) % 3;
+    switch(currentWaveform2)
+    {
+        case 0: osc2.SetWaveform(Oscillator::WAVE_POLYBLEP_SQUARE); break;
+        case 1: osc2.SetWaveform(Oscillator::WAVE_POLYBLEP_SAW); break;
+        case 2: osc2.SetWaveform(Oscillator::WAVE_POLYBLEP_TRI); break;
     }
 }
 
@@ -53,28 +74,45 @@ int main(void)
     hw.Init();
     hw.SetAudioBlockSize(4);
 
-    osc.Init(hw.AudioSampleRate());
-    osc.SetWaveform(Oscillator::WAVE_POLYBLEP_TRI);
+    // Initialize oscillators
+    osc1.Init(hw.AudioSampleRate());
+    osc2.Init(hw.AudioSampleRate());
+    osc1.SetWaveform(Oscillator::WAVE_POLYBLEP_TRI);
+    osc2.SetWaveform(Oscillator::WAVE_POLYBLEP_TRI);
 
-    GPIO button;
-    button.Init(D14, GPIO::Mode::INPUT, GPIO::Pull::PULLUP);
+    // Initialize buttons
+    GPIO button1, button2;
+    button1.Init(D14, GPIO::Mode::INPUT, GPIO::Pull::PULLUP);  // OSC1 waveform
+    button2.Init(D13, GPIO::Mode::INPUT, GPIO::Pull::PULLUP);  // OSC2 waveform
 
-    adcConfig[0].InitSingle(A0);
-    adcConfig[1].InitSingle(A1);
-    adcConfig[2].InitSingle(A2);
-    hw.adc.Init(adcConfig, 3);
+    // Configure ADC
+    adcConfig[0].InitSingle(A0);  // OSC1 Volume
+    adcConfig[1].InitSingle(A1);  // OSC1 Pitch
+    adcConfig[2].InitSingle(A2);  // OSC1 PWM
+    adcConfig[3].InitSingle(A3);  // OSC2 Volume
+    adcConfig[4].InitSingle(A4);  // OSC2 Pitch
+    adcConfig[5].InitSingle(A5);  // OSC2 PWM
+    hw.adc.Init(adcConfig, 6);
     hw.adc.Start();
 
     hw.StartAudio(AudioCallback);
 
     while(1)
     {
-        bool currentButtonState = !button.Read();
-        if(currentButtonState && !lastButtonState)
-        {
-            UpdateWaveform();
+        // Handle OSC1 button (D14)
+        bool currentButtonState1 = !button1.Read();  // Invert for active-low
+        if(currentButtonState1 && !lastButtonState1) {
+            UpdateWaveform1();
         }
-        lastButtonState = currentButtonState;
-        System::Delay(10);
+        lastButtonState1 = currentButtonState1;
+        
+        // Handle OSC2 button (D13)
+        bool currentButtonState2 = !button2.Read();  // Invert for active-low
+        if(currentButtonState2 && !lastButtonState2) {
+            UpdateWaveform2();
+        }
+        lastButtonState2 = currentButtonState2;
+        
+        System::Delay(10);  // Debounce delay
     }
 }
