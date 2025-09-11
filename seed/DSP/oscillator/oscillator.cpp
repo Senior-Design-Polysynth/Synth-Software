9fa7f4e36b78dd8bc3ce113a2c18ec19f0fa19b1
Jkakes9 <khanjordan2033@gmail.com>
2025-09-11 16:59:50 -0400

    first test: a lot of noise. Maybe Seeds are fried

--- seed/DSP/oscillator/oscillator.cpp
+++ seed/DSP/oscillator/oscillator.cpp
@@ -17,7 +17,7 @@ using namespace seed;
 
 // ===== Hardware =====
 DaisySeed hw;
-static constexpr int kNumVoices = 2;
+static constexpr int kNumVoices = 4;
 static constexpr int kNumKeys   = 6;               // hardware buttons
 //static const Pin kButtonPins[kNumKeys] = {D9, D10, D11, D12, D13, D14};
 //GPIO keybutton[kNumKeys];
@@ -246,7 +246,7 @@ static void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer
         }
     }
 
-    for(size_t i = 0; i < size; ++i)
+   for(size_t i = 0; i < size; ++i)
     {
         float mix = 0.f;
         for(int v = 0; v < kNumVoices; ++v)
@@ -337,11 +337,47 @@ int main(void)
         osc2[v].SetAmp(0.f);
     }
 
-    // Start audio
+    SaiHandle         sai2;
+    SaiHandle::Config sc;
+    sc.periph          = SaiHandle::Config::Peripheral::SAI_2;
+    sc.sr              = SaiHandle::Config::SampleRate::SAI_48KHZ;
+    sc.bit_depth       = SaiHandle::Config::BitDepth::SAI_24BIT;
+
+    sc.a_sync          = SaiHandle::Config::Sync::SLAVE;
+    sc.b_sync          = SaiHandle::Config::Sync::MASTER;
+    sc.a_dir           = SaiHandle::Config::Direction::RECEIVE;
+    sc.b_dir           = SaiHandle::Config::Direction::TRANSMIT;
+    sc.pin_config.fs   = seed::D27;
+    sc.pin_config.mclk = seed::D24;
+    sc.pin_config.sck  = seed::D28;
+    sc.pin_config.sb   = seed::D25;
+    sc.pin_config.sa   = seed::D26;
+
+    //Initialize the SAI new handle 
+    sai2.Init(sc);
+
+    //Reconfigure Audio for two codecs 
+    //
+    //  Default eurorack circuit has an extra 6dB headroom 
+    //  so the 0.5 here makes it so that a -1 to 1 audio signal
+    //  will correspond to a -5V to 5V (10Vpp) audio signal.
+    //  Audio will clip at -2 to 2, and result 20Vpp output.
+    //
+
+    AudioHandle::Config audio_cfg;
+    audio_cfg.blocksize  = 48;
+    audio_cfg.samplerate = SaiHandle::Config::SampleRate::SAI_48KHZ;
+    audio_cfg.postgain   = 0.5f;
+
+    // Initialize for two SAIs, including the built-in SAI that is 
+    //  configured during hw.Init()
+    //
+    hw.audio_handle.Init(audio_cfg, hw.AudioSaiHandle(), sai2);
+    // Finally start the audio
     hw.StartAudio(AudioCallback);
 
     // Poll buttons + process MIDI
-    bool prev[kNumKeys] = {false};
+    //bool prev[kNumKeys] = {false};
 
     while(true)
     {
